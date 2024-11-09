d09e7d68b4adbe3a8a83a4bd8a8009eb51da540d
nxhuel <nahueltisera03@gmail.com>
2024-11-09 18:33:26 -0300

    Avance de config autenticacion

---
+++ Aplicaciones/Autenticacion/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def singup(request):
+    if request.method == 'GET':
+        return render(request, "signup.html", {
+            'form': UserCreationForm,
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'],
+                    password=request.POST['password1']
+                )
+                user.save()
+                return HttpResponse('Usuario creado satisfactoriamente')
+            except:
+                return HttpResponse('Usuario ya existe')
+        return HttpResponse('Contraseñas no coinciden')
--- SistemaUtilesDemo/urls.py
+++ SistemaUtilesDemo/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Aplicaciones.AccesoAlSistema.urls'))
+    path('', include('Aplicaciones.AccesoAlSistema.urls')),
+    path('signup/', include('Aplicaciones.Autenticacion.urls'))
 ]
